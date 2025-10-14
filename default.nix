186a1d5d99d0d13d1d66ba2b05d9261502f9c241
cmdorexe <rych.91@gmail.com>
2025-10-14 15:15:39 +0300

    add my pkgs default.nix

--- default.nix
+++ default.nix
@@ -15,6 +15,14 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  far2l = pkgs.callPackage ./pkgs/by-name/fa/far2l/default.nix { };
+  _1c-enterprise-client = pkgs.callPackage ./pkgs/by-name/_1/_1c-enterprise-client/default.nix { };
+  _1c-enterprise-common = pkgs.callPackage ./pkgs/by-name/_1/_1c-enterprise-common/default.nix { };
+  _1c-enterprise-server = pkgs.callPackage ./pkgs/by-name/_1/_1c-enterprise-server/default.nix { };
+  _1c-enterprise = pkgs.callPackage ./pkgs/by-name/_1/_1c-enterprise/default.nix { };
+  _7zlib = pkgs.callPackage ./pkgs/by-name/_7/_7zlib/default.nix { };
+  waterfox = pkgs.callPackage ./pkgs/by-name/wa/waterfox/default.nix { };
+
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
